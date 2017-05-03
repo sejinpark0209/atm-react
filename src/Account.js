6d98e8044f8959d8a90c65ae2b629417ed0d8db6
sejinpark0209 <sejinpark0209@gmail.com>
2017-05-03 09:18:06 -0700

    completed without the zero

--- src/Account.js
+++ src/Account.js
@@ -1,14 +1,41 @@
 import React, { Component } from 'react';
 
+
+
 export default class Account extends Component {
+  constructor (props) {
+    super()
+    this.state = {
+      balance: 0
+    }
+  }
+  handleDepositClick (e) {
+    console.log("click button",e);
+    let amount = parseInt(this.refs.money.value);
+    let newAmount = this.state.balance + amount;
+    this.setState({
+      balance: newAmount
+    })
+    this.refs.money.value = '';
+  }
+  handleWithdrawClick (e) {
+    console.log("click button",e);
+    let amount = parseInt(this.refs.money.value);
+    let newAmount = this.state.balance - amount;
+    this.setState({
+      balance: newAmount
+    })
+    this.refs.money.value = '';
+  }
+
   render() {
     return (
       <div className="account">
-        <h2>TODO: CHANGE TO PROP</h2>
-        <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
-        <input type="button" value="Deposit" />
-        <input type="button" value="Withdraw" />
+        <h2>{this.props.name}</h2>
+        <div className="balance">${this.state.balance}</div>
+        <input ref = "money" type="text" placeholder="enter an amount" />
+        <input type="button" value="Deposit" onClick={(e) => this.handleDepositClick(e)}/>
+        <input type="button" value="Withdraw" onClick={(e) => this.handleWithdrawClick(e)}/>
       </div>
     )
   }
